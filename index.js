a85f5c8557827acf87d5487af39c8c4359ed09b4
billysullivan <31835804+billysullivan@users.noreply.github.com>
2023-11-08 09:14:52 +0000

    Push to github

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' }); // Set the destination for uploaded files
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +13,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// Add a new route to handle file uploads
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the 'upfile' file
+  // req.body will hold the text fields, if there were any
+  if (req.file) {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  } else {
+    res.json({ error: 'No file selected' });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
